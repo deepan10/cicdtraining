feacda087aa76a686d18dcacbfa423b75994cf8c
Deepankumar <deepan0433@gmail.com>
2019-06-26 19:34:37 +0200

    RF Library added

---
+++ integrationtests/library/CalcLib.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+class CalcLib:
+
+    def __init__(self):
+        self.result = 0
+
+    def perform_expr(self, expr, val_1, val_2):
+        self.result = float(subprocess.check_output(['calc', expr, val_1, val_2]))
+
+    def check_result(self, expected):
+        if self.result != float(expected):
+            print("Fail")
+            raise ArithmeticError()
+        else:
+            print("Success")
+
+    def reset_result(self):
+        self.result = 0
+
